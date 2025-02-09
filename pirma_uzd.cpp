88724cfd9aefd39577e7bbf3c59c5ef309b34e00
Povilascik <184389477+Povilascik@users.noreply.github.com>
2025-02-09 17:37:58 +0200

    1
    1

---
+++ pirma_uzd.cpp
@@ -0,0 +1,10 @@
+#include "libr.h"
+
+vector <duomenys> studentai;
+
+int main()
+{
+    read("C:/Users/PC/Documents/GitHub/pirmas/duomenys.txt",studentai);
+    write("C:/Users/PC/Documents/GitHub/pirmas/rez.txt",studentai);
+    return 0;
+}
