a265813ffaf8e49fa1727d6430ce3f4b4c598b04
Povilascik <184389477+Povilascik@users.noreply.github.com>
2025-02-21 09:48:36 +0200

    Pasirinkimas

--- pirma_uzd.cpp
+++ pirma_uzd.cpp
@@ -10,7 +10,8 @@ int main()
     cout << "norint ivesti duomenis ranka, iveskite 1. \n"
      << "norint sugeneruoti pazymius, iveskite 2. \n"
      << "norint sugeneruotus pazymius ir vardus, iveskite 3. \n"
-     << "norint baigti darba, iveskite 4.";
+     << "norint nuskaityti vardus is failo, iveskite 4. \n"
+     << "norint baigti darba, iveskite 5.";
     cin >> a;
 
     switch (a) {
@@ -27,11 +28,28 @@ int main()
         write("C:/Users/PC/Documents/GitHub/pirmas/rez.txt", studentai);
         break;
         case 4:
-            cout << "Darbas baigtas." << endl;
+            int pasirinkimas;
+        cout << "pasirinkite kuri faila norite nuskaityti: \n"
+        << "1. studentai10000.txt \n"
+        << "2. studentai100000.txt \n"
+        << "3. studentai1000000.txt \n";
+        cin>>pasirinkimas;
+        switch (pasirinkimas) {
+            case 1:
+                read("C:/Users/PC/Documents/GitHub/pirmas/studentai10000.txt", studentai);
+            break;
+            case 2:
+                read("C:/Users/PC/Documents/GitHub/pirmas/studentai100000.txt", studentai);
+            break;
+            case 3:
+                read("C:/Users/PC/Documents/GitHub/pirmas/studentai1000000.txt", studentai);
+            break;
+        }
+
+        ss_write("C:/Users/PC/Documents/GitHub/pirmas/kursiokai.txt", studentai);
         break;
         case 5:
-            read("C:/Users/PC/Documents/GitHub/pirmas/studentai1000000.txt", studentai);
-        ss_write("C:/Users/PC/Documents/GitHub/pirmas/kursiokai.txt", studentai);
+             cout << "Darbas baigtas." << endl;
         break;
         default:
             cout << "Neteisingas pasirinkimas." << endl;
